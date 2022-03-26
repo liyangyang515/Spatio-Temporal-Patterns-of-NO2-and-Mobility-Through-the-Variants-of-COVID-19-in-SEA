3567c224939546724bc4ceb43998891901fa64d7
liyangyang515 <yangyang.li@ntu.edu.sg>
2022-03-27 00:42:22 +0800

    update

---
+++ apps/SHAP.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
+import numpy as np
+import shap
+
+model = load_model('')
+
